5dafa3fc8224a6625cca49c7b54178537e7a7a6a
elkofix <holasoygermamwow@gmail.com>
2025-04-15 00:02:13 -0500

    feat: adding backend in s3 for both folders

--- terraform/microservices/main.tf
+++ terraform/microservices/main.tf
@@ -2,3 +2,12 @@ resource "azurerm_resource_group" "microservices_rg" {
   name     = var.resource_group_name
   location = var.location
 }
+
+terraform {
+  backend "s3" {
+    bucket         = "terraform-state-bucket-microservices"
+    key            = "microservices/terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+  }
+}
